4c9f2af7b20ac4e44cdcf9069e13409656a8c3bd
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-04-12 15:29:06 +0200

    Implement methods (unverified)

--- VectorImplementation/MyVector.hpp
+++ VectorImplementation/MyVector.hpp
@@ -8,6 +8,12 @@ namespace my {
 template <typename T>
 class my::Vector
 {
+private:
+	// Fields
+	int _size;
+	int _capacity;
+	T* _elements;
+
 public:
 	// Constructors and Destructor
 	Vector(int initial_capacity = 10);
@@ -15,29 +21,24 @@ public:
 	Vector(const Vector& source);
 	~Vector();
 
-	// Methods
+	// Methods (one-liners)
 	int size() const { return _size; }
 	int capacity() const { return _capacity; }
 	bool is_empty() const { return _size == 0; }
-	void clear();
+	T& back() const { return _elements[_size - 1]; }
+	T& front() const { return _elements[0]; }
+	void clear() { _size = 0; }
+
+	// Methods
 	void push_back(const T& element);
 	void pop_back();
 	void insert(int index, const T& element);
 	void erase(int index);
-	T& back() const;
-	T& front() const;
 
 	// Operators
 	T& operator [](int index) { return _elements[index]; }
 	const T& operator [](int index) const { return _elements[index]; }
 	Vector& operator =(const Vector& source);
-
-
-private:
-	// Fields
-	int _size;
-	int _capacity;
-	T* _elements;
 };
 
 template<typename T>
@@ -74,6 +75,71 @@ template<typename T>
 inline my::Vector<T>::~Vector()
 {
 	delete[] _elements;
+}
+
+template<typename T>
+inline void my::Vector<T>::push_back(const T& element)
+{
+	if (_size == _capacity) {
+		_capacity *= 2;
+		T* new_elements = new T[_capacity];
+		for (int i = 0; i < _size; i++) {
+			new_elements[i] = _elements[i];
+		}
+		delete[] _elements;
+		_elements = new_elements;
+	}
+
+	_elements[_size] = element;
+	_size++;
+}
+
+template<typename T>
+inline void my::Vector<T>::pop_back()
+{
+	if (_size > 0) {
+		_size--;
+	}
+}
+
+template<typename T>
+inline void my::Vector<T>::insert(int index, const T& element)
+{
+	if (index == _size) {
+		push_back(element);
+		return;
+	}
+	
+	if (_size == _capacity) {
+		_capacity *= 2;
+		T* new_elements = new T[_capacity];
+		for (int i = 0; i < index; i++) {
+			new_elements[i] = _elements[i];
+		}
+		new_elements[index] = element;
+		for (int i = index; i < _size; i++) {
+			new_elements[i + 1] = _elements[i];
+		}
+		delete[] _elements;
+		_elements = new_elements;
+		_size++;
+		return;
+	}
+
+	for (int i = _size - 1; i >= index; i--) {
+		_elements[i] = _elements[i - 1];
+	}
+	_elements[index - 1] = element;
+	_size++;
+}
+
+template<typename T>
+inline void my::Vector<T>::erase(int index)
+{
+	_size--;
+	for (int i = index; i < _size; i++) {
+		_elements[i] = _elements[i - 1];
+	}
 }
 
 template<typename T>
