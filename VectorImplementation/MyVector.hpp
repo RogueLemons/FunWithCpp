72de515f13086d108d381b1619bec907aef61b48
RogueLemons <60348181+RogueLemons@users.noreply.github.com>
2024-04-14 20:37:40 +0200

    Add homemade vector (#3)
    * Add base visual studio solution files
    
    * asd
    
    * Add vector class with function declarations and constructor definitions
    
    * Implement operators
    
    * Implement methods (unverified)
    
    * Add verification code (not completed)
    
    * asd
    
    * Fix bugs insert/erase bugs, add out-of-bounds indexing
    
    * ads
    
    * Throw exception when accessing element of empty vector
    
    * Add adjust_capacity()
    
    * asd
    
    * asd

---
+++ VectorImplementation/MyVector.hpp
@@ -0,0 +1,198 @@
+#pragma once
+
+namespace my {
+	template <typename T>
+	class Vector;
+}
+
+template <typename T>
+class my::Vector
+{
+private:
+	// Fields
+	int _size;
+	int _capacity;
+	T* _elements;
+
+	// Out-of-bounds indexing
+	int adjust_index(int index) const;
+
+public:
+	// Constructors and Destructor
+	Vector(int initial_capacity = 10);
+	Vector(int size, const T& inital_values);
+	Vector(const Vector& source);
+	~Vector();
+
+	// Methods (one-liners)
+	int size() const { return _size; }
+	int capacity() const { return _capacity; }
+	bool is_empty() const { return _size == 0; }
+	T& back() const { return _elements[_size - 1]; }
+	T& front() const { return _elements[0]; }
+	void clear() { _size = 0; }
+
+	// Methods
+	void push_back(const T& element);
+	void pop_back();
+	void insert(int index, const T& element);
+	void erase(int index);
+	void adjust_capacity();
+	void adjust_capacity(int capacity);
+
+	// Operators
+	T& operator [](int index) { return _elements[adjust_index(index)]; }
+	const T& operator [](int index) const { return _elements[adjust_index(index)]; }
+	Vector& operator =(const Vector& source);
+};
+
+template<typename T>
+inline int my::Vector<T>::adjust_index(int index) const
+{
+	if (is_empty())
+		throw std::invalid_argument("Cannot access elements of empty Vector.");
+	else if (index >= _size)
+		return adjust_index(index - _size);
+	else if (index < 0)
+		return adjust_index(_size + index);
+	else
+		return index;
+}
+
+template<typename T>
+inline my::Vector<T>::Vector(int initial_capacity)
+{
+	_size = 0;
+	_capacity = initial_capacity;
+	_elements = new T[_capacity];
+}
+
+template<typename T>
+inline my::Vector<T>::Vector(int size, const T& inital_values)
+{
+	_size = size;
+	_capacity = size * 2;
+	_elements = new T[_capacity];
+	for (int i = 0; i < _size; i++) {
+		_elements[i] = inital_values;
+	}
+}
+
+template<typename T>
+inline my::Vector<T>::Vector(const Vector& source)
+{
+	_size = source._size;
+	_capacity = source._capacity;
+	_elements = new T[_capacity];
+	for (int i = 0; i < _size; i++) {
+		_elements[i] = source._elements[i];
+	}
+}
+
+template<typename T>
+inline my::Vector<T>::~Vector()
+{
+	delete[] _elements;
+}
+
+template<typename T>
+inline void my::Vector<T>::push_back(const T& element)
+{
+	if (_size == _capacity)
+		adjust_capacity(_capacity * 2);
+
+	_elements[_size] = element;
+	_size++;
+}
+
+template<typename T>
+inline void my::Vector<T>::pop_back()
+{
+	if (_size > 0) {
+		_size--;
+	}
+}
+
+template<typename T>
+inline void my::Vector<T>::insert(int index, const T& element)
+{
+	index = adjust_index(index);
+
+	if (index == _size) {
+		push_back(element);
+		return;
+	}
+	
+	if (_size == _capacity) {
+		_capacity *= 2;
+		T* new_elements = new T[_capacity];
+		for (int i = 0; i < index; i++) {
+			new_elements[i] = _elements[i];
+		}
+		new_elements[index] = element;
+		for (int i = index; i < _size; i++) {
+			new_elements[i + 1] = _elements[i];
+		}
+		delete[] _elements;
+		_elements = new_elements;
+		_size++;
+		return;
+	}
+
+	for (int i = _size - 1; i >= index; i--) {
+		_elements[i] = _elements[i - 1];
+	}
+	_elements[index] = element;
+	_size++;
+}
+
+template<typename T>
+inline void my::Vector<T>::erase(int index)
+{
+	index = adjust_index(index);
+
+	_size--;
+	for (int i = index; i < _size; i++) {
+		_elements[i] = _elements[i + 1];
+	}
+}
+
+template<typename T>
+inline void my::Vector<T>::adjust_capacity()
+{
+	adjust_capacity(_size * 2);
+}
+
+template<typename T>
+inline void my::Vector<T>::adjust_capacity(int capacity)
+{
+	if (capacity < _size)
+		throw std::invalid_argument("Cannot decrease capacity lower than current size.");
+
+	_capacity = capacity;
+	T* new_elements = new T[_capacity];
+	for (int i = 0; i < _size; i++) {
+		new_elements[i] = _elements[i];
+	}
+	delete[] _elements;
+	_elements = new_elements;
+}
+
+template<typename T>
+inline my::Vector<T>& my::Vector<T>::operator=(const my::Vector<T>& source)
+{
+	if (source._size > _size) {
+		delete[] _elements;
+		_capacity = source._size * 2;
+		_elements = new T[_capacity];
+	}
+	
+	_size = source._size;
+	for (int i = 0; i < _size; i++) {
+		_elements[i] = source._elements[i];
+	}
+
+	return *this;
+}
+
+
---
+++ VectorImplementation/VectorImplementation.cpp
@@ -0,0 +1,83 @@
+// VectorImplementation.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include "MyVector.hpp"
+using std::cout;
+using std::endl;
+
+static void print(const my::Vector<double>& vec) {
+    cout << "Size: " << vec.size() << ", Cap: " << vec.capacity();
+    cout << ", Elements: [";
+    for (int i = 0; i < vec.size(); i++) {
+        cout << " " << vec[i];
+        if (i != vec.size() - 1)
+            cout << ",";
+    }
+    cout << " ] " << endl << endl;
+}
+
+int main()
+{
+    try {
+        cout << "--- Vector Implementation ---" << endl << endl;
+        
+        int initial_capacity = 5;
+        my::Vector<double> vec(initial_capacity);
+        cout << "Empty vector with initial capacity " << initial_capacity << ": " << endl;
+        print(vec);
+
+        cout << "Add some values:" << endl;
+        vec.push_back(1.2);
+        vec.push_back(2.4);
+        vec.push_back(3.6);
+        vec.push_back(4.8);
+        vec.push_back(6.0);
+        vec.push_back(7.2);
+        print(vec);
+
+        int index = 2;
+        cout << "Erase at index " << index << ":" << endl;
+        vec.erase(index);
+        print(vec);
+
+        index = 1;
+        cout << "Insert at index " << index << ":" << endl;
+        vec.insert(index, 7.7);
+        print(vec);
+
+        index = 8;
+        while (vec.size() != vec.capacity())
+            vec.push_back(42);
+        double value = 7.7;
+        cout << "Insert " << value << " at index " << index << " (with capacity increase) : " << endl;
+        vec.insert(index, 7.7);
+        print(vec);
+
+        cout << "Get out-of-bounds elements " << -1 << ", " << vec.size() << ", " << 2 * vec.size() + 3 << ":" << endl;
+        cout << vec[-1] << ", " << vec[vec.size()] << ", " << vec[2 * vec.size() + 3] << endl << endl;
+
+        while (vec.size() > 3)
+            vec.pop_back();
+        cout << "Pop values until size is 3" << endl;
+        print(vec);
+
+        cout << "Free up memory by reducing capacity:" << endl;
+        vec.adjust_capacity();
+        print(vec);
+
+    }
+    catch (const std::exception& e) {
+        cout << e.what() << endl;
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
