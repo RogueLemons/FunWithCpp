de1d6bfc5f3da864b761c783c3c699e0650ef420
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-04-16 10:46:14 +0200

    Refactor main

--- VectorImplementation/VectorImplementation.cpp
+++ VectorImplementation/VectorImplementation.cpp
@@ -5,6 +5,9 @@
 using std::cout;
 using std::endl;
 
+static void test_my_simple_vector();
+static void test_my_modular_vector();
+
 template <typename T>
 static void print(const my::Vector<T>& vec) {
     cout << "Size: " << vec.size() << ", Cap: " << vec.capacity();
@@ -20,52 +23,7 @@ static void print(const my::Vector<T>& vec) {
 int main()
 {
     try {
-        cout << "--- Vector Implementation ---" << endl << endl;
-        
-        int initial_capacity = 5;
-        my::Vector<double> vec(initial_capacity);
-        cout << "Empty vector with initial capacity " << initial_capacity << ": " << endl;
-        print(vec);
-
-        cout << "Add some values:" << endl;
-        vec.push_back(1.2);
-        vec.push_back(2.4);
-        vec.push_back(3.6);
-        vec.push_back(4.8);
-        vec.push_back(6.0);
-        vec.push_back(7.2);
-        print(vec);
-
-        int index = 2;
-        cout << "Erase at index " << index << ":" << endl;
-        vec.erase(index);
-        print(vec);
-
-        index = 1;
-        cout << "Insert at index " << index << ":" << endl;
-        vec.insert(index, 7.7);
-        print(vec);
-
-        index = 8;
-        while (vec.size() != vec.capacity())
-            vec.push_back(42);
-        double value = 7.7;
-        cout << "Insert " << value << " at index " << index << " (with capacity increase) : " << endl;
-        vec.insert(index, 7.7);
-        print(vec);
-
-        cout << "Get out-of-bounds elements " << -1 << ", " << vec.size() << ", " << 2 * vec.size() + 3 << ":" << endl;
-        cout << vec[-1] << ", " << vec[vec.size()] << ", " << vec[2 * vec.size() + 3] << endl << endl;
-
-        while (vec.size() > 3)
-            vec.pop_back();
-        cout << "Pop values until size is 3" << endl;
-        print(vec);
-
-        cout << "Free up memory by reducing capacity:" << endl;
-        vec.adjust_capacity();
-        print(vec);
-
+        test_my_simple_vector();
     }
     catch (const std::exception& e) {
         cout << e.what() << endl;
@@ -82,3 +40,52 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+void test_my_simple_vector()
+{
+    cout << "\n--- Simple Vector with Manual Capacity Resizing  ---" << endl << endl;
+
+    int initial_capacity = 5;
+    my::Vector<double> vec(initial_capacity);
+    cout << "Empty vector with initial capacity " << initial_capacity << ": " << endl;
+    print(vec);
+
+    cout << "Add some values:" << endl;
+    vec.push_back(1.2);
+    vec.push_back(2.4);
+    vec.push_back(3.6);
+    vec.push_back(4.8);
+    vec.push_back(6.0);
+    vec.push_back(7.2);
+    print(vec);
+
+    int index = 2;
+    cout << "Erase at index " << index << ":" << endl;
+    vec.erase(index);
+    print(vec);
+
+    index = 1;
+    cout << "Insert at index " << index << ":" << endl;
+    vec.insert(index, 7.7);
+    print(vec);
+
+    index = 8;
+    while (vec.size() != vec.capacity())
+        vec.push_back(42);
+    double value = 7.7;
+    cout << "Insert " << value << " at index " << index << " (with capacity increase) : " << endl;
+    vec.insert(index, 7.7);
+    print(vec);
+
+    cout << "Get out-of-bounds elements " << -1 << ", " << vec.size() << ", " << 2 * vec.size() + 3 << ":" << endl;
+    cout << vec[-1] << ", " << vec[vec.size()] << ", " << vec[2 * vec.size() + 3] << endl << endl;
+
+    while (vec.size() > 3)
+        vec.pop_back();
+    cout << "Pop values until size is 3" << endl;
+    print(vec);
+
+    cout << "Free up memory by reducing capacity:" << endl;
+    vec.adjust_capacity();
+    print(vec);
+}
