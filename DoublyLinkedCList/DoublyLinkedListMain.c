85d7906f19b00f368e0fa811f347c0918ee11f67
ludvig.langebro@gmail.com <ludvig.langebro@gmail.com>
2024-04-20 08:54:32 +0200

    Free up memory before exiting main()

--- DoublyLinkedCList/DoublyLinkedListMain.c
+++ DoublyLinkedCList/DoublyLinkedListMain.c
@@ -21,35 +21,36 @@ int main() {
 	printf("---Testing List of Nodes---\n");
 
 	printf("\nAppend and Prepend nodes:\n");
-	Node* list_node = new_node(1);
-	list_node->append(list_node, new_node(2));
-	list_node->append(list_node, new_node(3));
-	list_node->append(list_node, new_node(4));
-	list_node->append(list_node, new_node(5));
-	list_node->prepend(&list_node, new_node(0));
-	list_node->prepend(&list_node, new_node(-1));
-	print_node_list(list_node);
+	Node* linked_nodes = new_node(1);
+	linked_nodes->append(linked_nodes, new_node(2));
+	linked_nodes->append(linked_nodes, new_node(3));
+	linked_nodes->append(linked_nodes, new_node(4));
+	linked_nodes->append(linked_nodes, new_node(5));
+	linked_nodes->prepend(&linked_nodes, new_node(0));
+	linked_nodes->prepend(&linked_nodes, new_node(-1));
+	print_node_list(linked_nodes);
 
 	int index = 2;
-	printf("\nElement at index %d is:\t%d \n", index, node_at_index(list_node, index)->value);
-	printf("The first element is:\t%d \n", node_first(list_node)->value);
-	printf("The last element is:\t%d \n", node_last(list_node)->value);
+	printf("\nElement at index %d is:\t%d \n", index, node_at_index(linked_nodes, index)->value);
+	printf("The first element is:\t%d \n", node_first(linked_nodes)->value);
+	printf("The last element is:\t%d \n", node_last(linked_nodes)->value);
 
 	printf("\nFree up memory from third and subsequent nodes:\n");
-	delete_nodes(list_node->next->next);
-	print_node_list(list_node);
+	delete_nodes(linked_nodes->next->next);
+	print_node_list(linked_nodes);
 
 	printf("\nInsert three 3s after the first node:\n");
 	Node* more_nodes = new_node(3);
 	node_append(more_nodes, new_node(3));
 	node_append(more_nodes, new_node(3));
-	list_node->insert_next(list_node, more_nodes);
-	print_node_list(list_node);
+	linked_nodes->insert_next(linked_nodes, more_nodes);
+	print_node_list(linked_nodes);
 
 	int value = 9;
 	printf("\nInsert %d at index %d:\n", value, index);
-	node_insert_at(list_node, index, new_node(value));
-	print_node_list(list_node);
+	node_insert_at(linked_nodes, index, new_node(value));
+	print_node_list(linked_nodes);
 
+	delete_nodes(linked_nodes);
 	return 0;
 }
